d3717a4302bf8972aa9328c5d4ca1d100007085d
Chad Roesler <ChadRoesler@Me.com>
2026-08-01 22:05:22 -0500

    update correction

--- SerenMemory/seren_memory/app.py
+++ SerenMemory/seren_memory/app.py
@@ -52,7 +52,6 @@ from .routes import long as long_routes
 from .routes import search as search_routes
 
 from seren_meninges import get_version
-from seren_meninges.updates import updates_payload
 from seren_meninges.auth import bearer_auth_middleware
 from seren_meninges.viewer import render_from_dir
 from seren_sinew.request_log import RequestLoggingMiddleware
